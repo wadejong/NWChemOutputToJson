76477a9c88319f580845640f851919b6e9acbb79
Bert de Jong <wadejong@lbl.gov>
2015-11-15 11:18:23 -0800

    Adding progress print

--- NWChemJsonConversion.py
+++ NWChemJsonConversion.py
@@ -8,6 +8,7 @@ else:
 for files in range(start,len(sys.argv)):
   fileIn = open(sys.argv[files],'r')
   fileOut = open(sys.argv[files]+'.json','w')
+  print('Converting file ',files)
   jsonObj = nwchemToJson(argument)
   fileOut.write(jsonObj.convert(fileIn))
   fileIn.close()
