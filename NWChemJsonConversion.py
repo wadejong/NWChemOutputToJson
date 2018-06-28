7feb146a00d9562dcb9b35ecb6941e0db009c58d
Bert de Jong <wadejong@lbl.gov>
2018-06-28 16:35:45 -0700

    Update NWChemJsonConversion.py

--- NWChemJsonConversion.py
+++ NWChemJsonConversion.py
@@ -1,3 +1,14 @@
+##############################################################################
+# This source file is part of the NWChemOutputToJson project.
+# Copyright 2013-2014 U.C. Regents...
+# This source code is released under the New BSD License, (the "License").
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+
 from nwchemToJson import *
 if sys.argv[1] == 'noOrbitals':
   start = 2
