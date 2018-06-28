9acf5f7138d8e266d6d07f17168608a146a8a85b
Bert de Jong <wadejong@lbl.gov>
2018-06-28 16:49:36 -0700

    Update nwchemToJson.py

--- nwchemToJson.py
+++ nwchemToJson.py
@@ -1,3 +1,16 @@
+##############################################################################
+# This source file is part of the NWChemOutputToJson project.
+# Copyright (c) 2016, The Regents of the University of California, through 
+# Lawrence Berkeley National Laboratory (subject to receipt of any required 
+# approvals from the U.S. Dept. of Energy).
+# This source code is released under the BSD 3-Clause License, (the "License").
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+
 import json, sys, string, itertools
 from collections import OrderedDict
 
