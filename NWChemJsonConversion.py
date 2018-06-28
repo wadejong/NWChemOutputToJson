1dc97505dd3166faeebf79171e557b0a0666bc20
Bert de Jong <wadejong@lbl.gov>
2018-06-28 16:49:13 -0700

    Update NWChemJsonConversion.py

--- NWChemJsonConversion.py
+++ NWChemJsonConversion.py
@@ -1,7 +1,9 @@
 ##############################################################################
 # This source file is part of the NWChemOutputToJson project.
-# Copyright 2013-2014 U.C. Regents...
-# This source code is released under the New BSD License, (the "License").
+# Copyright (c) 2016, The Regents of the University of California, through 
+# Lawrence Berkeley National Laboratory (subject to receipt of any required 
+# approvals from the U.S. Dept. of Energy).
+# This source code is released under the BSD 3-Clause License, (the "License").
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
